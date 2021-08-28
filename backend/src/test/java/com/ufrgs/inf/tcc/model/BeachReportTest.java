6af6b8938915d7f986cba8ad0d80f7a81cbcaa4a
MateusCardoso <mateus.c.silva@hotmail.com>
2021-08-28 12:13:26 -0300

    Fix broken unit test for due to remove field

--- backend/src/test/java/com/ufrgs/inf/tcc/model/BeachReportTest.java
+++ backend/src/test/java/com/ufrgs/inf/tcc/model/BeachReportTest.java
@@ -13,45 +13,44 @@ public class BeachReportTest {
  
     @Test
 	public void equalsShouldReturnTrueForTheSameObjects() {
-		BeachReport left = new BeachReport(3L, 28.5, "Boa", 8);
+		BeachReport left = new BeachReport(3L, 28.5, "Boa");
 		assertTrue(left.equals(left));
 	}
 
 	@Test
 	public void equalsShouldReturnTrueForEqualObjects() {
-		BeachReport left = new BeachReport(3L, 28.5, "Boa", 8);
-		BeachReport right = new BeachReport(3L, 28.5, "Boa", 8);
+		BeachReport left = new BeachReport(3L, 28.5, "Boa");
+		BeachReport right = new BeachReport(3L, 28.5, "Boa");
 		assertTrue(left.equals(right));
 	}
 
 	@Test
 	public void equalsShouldReturnFalseForNonEqualObjects() {
-		BeachReport left = new BeachReport(3L, 28.5, "Boa", 8);
-		BeachReport right = new BeachReport(2L, 18.5, "Suja", 5);
+		BeachReport left = new BeachReport(3L, 28.5, "Boa");
+		BeachReport right = new BeachReport(2L, 18.5, "Suja");
 		assertFalse(left.equals(right));
 	}
 
 	@Test
 	public void equalsShouldReturnFalseForNull() {
-		BeachReport left = new BeachReport(3L, 28.5, "Boa", 8);
+		BeachReport left = new BeachReport(3L, 28.5, "Boa");
 		assertFalse(left.equals(null));
 	}
 
 	@Test
 	public void hashCodeShouldReturnTheSameHashCodeForEqualObjects() {
-		BeachReport left = new BeachReport(3L, 28.5, "Boa", 8);
-		BeachReport right = new BeachReport(3L, 28.5, "Boa", 8);
+		BeachReport left = new BeachReport(3L, 28.5, "Boa");
+		BeachReport right = new BeachReport(3L, 28.5, "Boa");
 		assertEquals(left.hashCode(), right.hashCode());
 	}
 
 	@Test
 	public void toStringShouldReturnAStringThatContainsAttributes() {
-		BeachReport report = new BeachReport(373L, 28.5, "Boa", 8);
+		BeachReport report = new BeachReport(373L, 28.5, "Boa");
 		String toString = report.toString();
 		assertTrue(toString.indexOf("373") > -1);
 		assertTrue(toString.indexOf("28.5") > -1);
         assertTrue(toString.indexOf("Boa") > -1);
-        assertTrue(toString.indexOf("8") > -1);
 	}
 
 

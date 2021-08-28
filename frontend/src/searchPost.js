8fbb5ca2c704f1794da61f88d63a99070d80f440
MateusCardoso <mateus.c.silva@hotmail.com>
2021-08-28 15:08:55 -0300

    Adds intial table layout with get request to current posts, placeholder pagination component

--- frontend/src/searchPost.js
+++ frontend/src/searchPost.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import {
   Header,
-  Segment
+  Pagination,
+  Segment,
+  Table,
+  TableCell
 } from 'semantic-ui-react'
 
 class SearchPost extends React.Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    async componentDidMount() {
+        const requestOptions = {
+            method: 'GET'
+        };
+        const response = await fetch(process.env.REACT_APP_API_URL+'/posts', requestOptions);
+        const data = await response.json();
+        var posts = [];
+        for(const post of data){
+            posts.push({
+                id: post.id,
+                description: post.description
+            })
+        }
+        this.setState({
+            posts: posts
+        });
+    }
+
+    renderPosts(props){
+        const posts = props;
+        return(
+            posts.map((post)=>
+                <Table.Row>
+                    <Table.Cell content={post.id}></Table.Cell>
+                    <Table.Cell content={post.description}></Table.Cell>
+                </Table.Row>
+        ))
     }
 
     render(){
@@ -15,6 +50,29 @@ class SearchPost extends React.Component{
             <div>
                 <Segment padded='very' inverted color='grey'>
                     <Header as='h1' content='Buscar Posts' textAlign='left' />
+                </Segment>
+                <Segment>
+                    <Header as='h4' content='Filtros:' textAlign='left' />
+                </Segment>
+                <Segment>
+                    <Table selectable>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>Titulo do Post</Table.HeaderCell>
+                                <Table.HeaderCell>Descriçao do Post</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+
+                        <Table.Body children>
+                            {this.renderPosts(this.state.posts)}
+                        </Table.Body>
+
+                        <Table.Footer>
+                            <Pagination>
+
+                            </Pagination>
+                        </Table.Footer>
+                    </Table>
                 </Segment>
             </div>
         );

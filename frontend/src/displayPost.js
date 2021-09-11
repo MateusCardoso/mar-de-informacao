ef8fff93c4961cec0cd166295dc65e2d7b7d7216
MateusCardoso <mateus.c.silva@hotmail.com>
2021-09-11 14:16:16 -0300

    Display post functional component implementation, retrieves post information

--- frontend/src/displayPost.js
+++ frontend/src/displayPost.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import {
   Header,
   Segment,
@@ -6,120 +6,146 @@ import {
   Grid
 } from 'semantic-ui-react'
 
-class DisplayPost extends React.Component{
+import { useParams } from 'react-router';
 
-    render(){
-        return(
-            <div>
-                <Segment padded='very' inverted color='grey'>
-                    <Header as='h1'>Posts Museu do Mar</Header>
-                </Segment>
-                <Segment>
-                    <Grid columns={2} stackable>
-                        <Grid.Column>
-                            <Segment basic >
-                                <Image size='big' src={'/images/placeholder.png'}></Image>
-                                
-                            </Segment>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Segment basic>Bom dia 13 de abril praia do quintao rei do peixe rs temperatura
-                                20 graus sem vento mar lavando meia praia corrente de sul forte valeu galera tamo junto
-                                <Segment basic>Tags: </Segment>
-                            </Segment>
-                            <Segment basic>
-                                <Header as='h3'>Situaçao do Mar</Header>
-                                <Segment>
-                                    <Segment basic>
-                                        Transparencia da Agua:
-                                    </Segment>
-                                
-                                    <Segment basic>
-                                        Sentido da Mare:
-                                    </Segment>
-                                    <Segment basic>
-                                        Intensidade da Mare:
-                                    </Segment>
-                                    <Segment basic>
-                                        Temperatura:
-                                    </Segment>    
-                                </Segment>
-                            </Segment>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Segment basic>
-                                <Header as='h3'>Situaçao do Vento</Header>
-                                <Segment>
-                                    <Segment basic>
-                                        Direçao do Vento:
-                                    </Segment>
-                                    <Segment basic>
-                                        Velocidade do Vento:
-                                    </Segment>
-                                </Segment>
-                            </Segment>
-                            <Segment basic>
-                                <Header as='h3'>Precipitaçao</Header>
-                                <Segment>
-                                    <Segment basic>
-                                        Quantidade de chuva (mm):
-                                    </Segment>
-                                    <Segment basic>
-                                        Velocidade do Vento:
-                                    </Segment>
-                                </Segment>
-                            </Segment>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Segment basic>
-                                <Header as='h3'>Pescaria</Header>
-                                <Segment>
-                                    <Segment basic>
-                                        Especie mais capturada:
-                                    </Segment>
-                                    <Segment basic>
-                                        Quantidade (Kg):
-                                    </Segment>
-                                </Segment>
-                            </Segment>
-                        </Grid.Column>
-                    </Grid>
-                </Segment>
-                <Segment>
-                    <Grid columns={2} stackable>
-                        <Grid.Column>
-                            <Header as='h3'>Animais Encontrados</Header>
+function DisplayPost(){
+
+    const [post, setPost] = useState({
+        postId: null,
+        description: '',
+        beachReport: {
+            waterQuality: '',
+            temperature: '',
+                windStatus: {
+                windDirection: '',
+                windVelocity: ''
+            }
+        },
+        links: [],
+        tags: []
+    });
+    
+    let {postId} = useParams();
+    const retrievePost = useCallback(async ()=>{
+        const requestOptions = {
+            method: 'GET'
+        };
+        const response = await fetch(process.env.REACT_APP_API_URL+'/posts/'+postId, requestOptions);
+        const data = await response.json();
+        setPost(data);
+    }, [postId]);
+    
+    useEffect(()=>{
+        retrievePost();
+    }, [retrievePost]);
+
+    return <div>
+            <Segment padded='very' inverted color='grey'>
+                <Header as='h1'>Posts Museu do Mar</Header>
+            </Segment>
+            <Segment>
+                <Grid columns={2} stackable>
+                    <Grid.Column>
+                        <Segment basic >
+                            <Image size='big' src={'/images/placeholder.png'}></Image>
+                            
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Segment basic>{post.description}
+                            <Segment basic>Tags: </Segment>
+                        </Segment>
+                        <Segment basic>
+                            <Header as='h3'>Situaçao do Mar</Header>
                             <Segment>
                                 <Segment basic>
-                                    Especies:
+                                    Transparencia da Agua: {post.beachReport.waterQuality}
+                                </Segment>
+                            
+                                <Segment basic>
+                                    Sentido da Mare:
+                                </Segment>
+                                <Segment basic>
+                                    Intensidade da Mare:
+                                </Segment>
+                                <Segment basic>
+                                    Temperatura: {post.beachReport.temperature} ºC
+                                </Segment>    
+                            </Segment>
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Segment basic>
+                            <Header as='h3'>Situaçao do Vento</Header>
+                            <Segment>
+                                <Segment basic>
+                                    Direçao do Vento: {post.beachReport.windStatus.windDirection}
+                                </Segment>
+                                <Segment basic>
+                                    Velocidade do Vento: {post.beachReport.windStatus.windVelocity} Km/h
+                                </Segment>
+                            </Segment>
+                        </Segment>
+                        <Segment basic>
+                            <Header as='h3'>Precipitaçao</Header>
+                            <Segment>
+                                <Segment basic>
+                                    Quantidade de chuva (mm):
+                                </Segment>
+                                <Segment basic>
+                                    Velocidade do Vento:
+                                </Segment>
+                            </Segment>
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Segment basic>
+                            <Header as='h3'>Pescaria</Header>
+                            <Segment>
+                                <Segment basic>
+                                    Especie mais capturada:
+                                </Segment>
+                                <Segment basic>
+                                    Quantidade (Kg):
+                                </Segment>
+                            </Segment>
+                        </Segment>
+                    </Grid.Column>
+                </Grid>
+            </Segment>
+            <Segment>
+                <Grid columns={2} stackable>
+                    <Grid.Column>
+                        <Header as='h3'>Animais Encontrados</Header>
+                        <Segment>
+                            <Segment basic>
+                                Especies:
+                            </Segment>
+                            <Segment basic>
+                                Fotos:
+                            </Segment>
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Header as='h3'>Lixo</Header>
+                            <Segment>
+                                <Segment basic>
+                                    Quantidade de Lixo (Kg):
+                                </Segment>
+                                <Segment basic>
+                                    Origem:
                                 </Segment>
                                 <Segment basic>
                                     Fotos:
                                 </Segment>
                             </Segment>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Header as='h3'>Lixo</Header>
-                                <Segment>
-                                    <Segment basic>
-                                        Quantidade de Lixo (Kg):
-                                    </Segment>
-                                    <Segment basic>
-                                        Origem:
-                                    </Segment>
-                                    <Segment basic>
-                                        Fotos:
-                                    </Segment>
-                                </Segment>
-                        </Grid.Column>
-                    </Grid>
-                </Segment>
-                <Segment>
-                    <Header as='h3'>Links:</Header>
-                </Segment>
-            </div>
-        );
-    }
+                    </Grid.Column>
+                </Grid>
+            </Segment>
+            <Segment>
+                <Header as='h3'>Links:</Header>
+            </Segment>
+        </div>;
 }
 
 export default DisplayPost

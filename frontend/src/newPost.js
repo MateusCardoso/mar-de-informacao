056ca4053114b386cf222da4c403f796585a455b
MateusCardoso <mateus.c.silva@hotmail.com>
2021-09-22 20:30:36 -0300

    Change newPost to functional component, trigger redirect to edit app after post creation

--- frontend/src/newPost.js
+++ frontend/src/newPost.js
@@ -1,98 +1,9 @@
-import React from 'react'
-import {
-  Form,
-  Grid,
-  Header,
-  Segment
-} from 'semantic-ui-react'
+import { useCallback, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-import PostText from './postText'
-import SavePostButton from './savePostButton'
-import BeachReport from './beachReport'
-import WindStatus from './windStatus'
-import LinkTable from './linkTable'
-import TagMultiselect from './tagMultiselect'
+function NewPost(){
 
-const style = {
-  h1: {
-    marginTop: '3em',
-  },
-  h2: {
-    margin: '4em 0em 2em',
-  },
-  h3: {
-    marginTop: '2em',
-    padding: '2em 0em',
-  },
-  last: {
-    marginBottom: '300px',
-  },
-}
-
-class NewPost extends React.Component{
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      postId: null,
-      title: '',
-      description: '',
-      beachReport: {
-        reportId: null,
-        waterQuality: '',
-        temperature: ''
-      },
-      windStatus: {
-        windId: null,
-	      windDirection: '',
-        windVelocity: ''
-      },
-      links: [],
-      tags: []
-    };
-    this.updatePostText = this.updatePostText.bind(this);
-    this.updateBeachReport = this.updateBeachReport.bind(this);
-    this.updateWindStatus = this.updateWindStatus.bind(this);
-    this.updateLinksTable = this.updateLinksTable.bind(this);
-    this.updateTagsList = this.updateTagsList.bind(this);
-  }
-
-  updatePostText(name, value) {
-    var updatedState = this.state;
-    updatedState[name] = value;
-    this.setState(updatedState);
-  }
-
-  updateBeachReport(name,value) {
-    var updatedReport = this.state.beachReport;
-    updatedReport[name] = value;
-    this.setState({beachReport: updatedReport})
-  }
-
-  updateWindStatus(name,value) {
-    var updatedWindStatus = this.state.windStatus;
-    updatedWindStatus[name] = value;
-    this.setState({windStatus: updatedWindStatus})
-  }
-
-  updateLinksTable(links){
-    this.setState({links: links});
-  }
-
-  updateTagsList(tags){
-    this.setState({tags: tags});
-  }
-
-  async componentDidMount() {
-    const entityIds = await this.createPostRecord();
-    this.setState({ 
-      postId: entityIds.postId,
-      beachReport: {reportId: entityIds.reportId},
-      windStatus:{windId: entityIds.windId}
-    });
-  }
-
-  async createPostRecord(){
+  const createPostRecord = useCallback ( async () => {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -104,40 +15,22 @@ class NewPost extends React.Component{
     };
     const response = await fetch(process.env.REACT_APP_API_URL+'/posts', requestOptions);
     const data = await response.json();
-    return ({postId: data.id, reportId: data.beachReport.id, windId: data.beachReport.windStatus.id});
-  }
+    return(data.id);
+  });
+  
+  const history = useHistory();
+  const redirectToEdit = useCallback( async (id) => {
+    const postId = await id;
+    if(postId !== null){
+      history.push("/Edit/" + postId);
+    }
+  });
 
-  render() {
-    return (
-      <div>
-        <Segment padded='very' inverted color='grey'>
-          <Header as='h1'>Entrar novo Post</Header>
-        </Segment>
-        <Form>
-          <Grid columns={2} stackable>
-            <PostText updatePostText={this.updatePostText} description={this.state.description}></PostText>
-            <BeachReport updateBeachReport={this.updateBeachReport}></BeachReport>
-            <WindStatus updateWindStatus={this.updateWindStatus}></WindStatus>
-            <TagMultiselect updateTagsList={this.updateTagsList} allowAdditions={true}></TagMultiselect>
-            <LinkTable links={this.state.links} postId={this.state.postId}></LinkTable>
-            <SavePostButton
-              postId={this.state.postId}
-              title={this.state.title}
-              description={this.state.description}
-              beachReport={this.state.beachReport}
-              windStatus={this.state.windStatus}
-              links={this.state.links}
-              tags={this.state.tags}
-              updateLinksTable={this.updateLinksTable}
-            >
-            </SavePostButton>
-            
-          </Grid>
-        </Form>
-      </div>
-    )
-  }
+  useEffect(()=>{
+    redirectToEdit(createPostRecord());
+  }, []);
 
+  return (null);
 }
 
 export default NewPost
